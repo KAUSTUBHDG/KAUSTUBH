e573d82edc7137b636e2d324b8b6398926896a0c
KAUSTUBHDG <63150992+KAUSTUBHDG@users.noreply.github.com>
2020-04-14 06:33:09 +0530

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Stack
+    {
+        private readonly List<Object> list=new List<object>();
+
+       
+        public void Push(Object obj )
+        {
+            if (obj==null)
+            {
+                throw new InvalidOperationException("Invalid input");
+            }
+            list.Add(obj);
+
+        }
+
+        public object Pop()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Cant pop from empty list");
+
+            list.RemoveAt(list.Count - 1);
+
+            return (list);
+        }
+
+      
+
+        public void Clear()
+        {
+            list.Clear();
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            Console.WriteLine(stack);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            Console.WriteLine(stack.Pop());
+            Console.ReadLine();
+        }
+    }
+}
