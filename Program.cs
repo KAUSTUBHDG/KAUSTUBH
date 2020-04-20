bf09bb70165f6e046d9e999acb9bf75bf2d025ae
KAUSTUBHDG <63150992+KAUSTUBHDG@users.noreply.github.com>
2020-04-20 17:02:22 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,114 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
+﻿    using System;
+using System.Data.Common;
+using System.Threading;
 
-namespace ConsoleApp4
-{
-    public interface ITask
+namespace ConsoleApp3
     {
-        void Execute();
-    }
-
-    public interface IWorkflow
-    {
-        void Add(ITask task);
-        void Remove(ITask task);
-        IEnumerable<ITask> GetTasks();
-    }
-
-    class VideoUploader:ITask
-    {
-        public void Execute()
+        public abstract class DbConnection
         {
-            Console.WriteLine("Uploading Video");
-        }
-    }
+            private string ConnectionString { get; set; }
+            private TimeSpan TimeOut { get; set; }
 
-    class CallWebService:ITask
-    {
-        public void Execute()
-        {
-            Console.WriteLine("Calling web services");
-        }
-    }
-
-    class SendMail:ITask
-    {
-        public void Execute()
-        {
-            Console.WriteLine("Sending an Email");
-        }
-    }
-
-    class ChangeStatus:ITask
-    {
-        public void Execute()
-        {
-            Console.WriteLine("Changing Status");
-        }
-    }
-
-
-
-    public class Workflow:IWorkflow 
-    {
-        private readonly List<ITask> _task;
-
-        public Workflow()
-        {
-            _task = new List<ITask>();
-        }
-
-        public void Add(ITask task)
-        {
-            _task.Add(task);
-        }
-
-        public void Remove(ITask task)
-        {
-            _task.Remove(task);
-        }
-
-            
-
-        public IEnumerable<ITask> GetTasks()
-        {
-            return _task;
-        }
-
-    }
-
-
-
-    public class WorkFlowEngine
-    {
-        public void Run(IWorkflow workflow)
-        {
-
-            foreach (ITask I in workflow.GetTasks())
+            public DbConnection(string connectionString)
             {
-                I.Execute();
+                var timeout = TimeOut;
+                ConnectionString = connectionString;
+            if (String.IsNullOrEmpty(connection))
+                {
+                    Console.WriteLine("Cannot pass an empty string!!!");
+                }
+
             }
+
+        public TimeSpan Time(int time)
+        {
+            time = TimeOut;
+            Thread.Sleep(time);
         }
 
+        public abstract void Opening();
+        public abstract void Closing();
+          
+        }
+
+    public class SqlConnection : DbConnection
+    {
+        private readonly string connectionString;
+
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+          
+        }
+
+
+        public override void Closing()
+        {
+            Console.WriteLine("Closing SQL connection");  
+        }
+
+        public override void Opening()
+        {
+            Console.WriteLine("Opening SQL connection");
+        }
     }
 
+    public class  OracleConnection:DbConnection
+    {
+        public OracleConnection(string connectionString):base(connectionString)
+        {
 
+        }
+        public override void Closing()
+        {
+            Console.WriteLine("Closing Oracle connection");
+        }
+
+        public override void Opening()
+        {
+            Console.WriteLine("Opening Oracle connection");
+        }
+    }    
+
+
+    class DbCommand
+    {
+        private DbConnection _dbConnection { get; set; }
+        private string _command { get; set; }
+        private DbConnection _time { get; set; }
+
+        public DbCommand(DbConnection dbConnection,string command,DbConnection Timeout)
+        {
+            _dbConnection = dbConnection;
+            _command = command;
+            _time = Timeout;
+        }
+
+ 
+        public void Execute()
+        {
+            _dbConnection.Opening();
+            _time.Time(100);
+            _dbConnection.Closing();
+            _time.Time(100);
+        }
+    }
 
     class Program
     {
-        static void Main(string[] args)
-        {
-            var ITask = new Workflow();
-            ITask.Add(new VideoUploader());
-            ITask.Add(new SendMail());
-            ITask.Add(new CallWebService());
-            ITask.Add(new ChangeStatus());
+       static void Main(string[] args)
+       {
+            Console.WriteLine("Tyep SQL or ORACLE to connect to.");
+            var input = Console.ReadLine().ToLower();
 
-            var engine = new WorkFlowEngine();
-            engine.Run(ITask);
-
-            Console.ReadLine();
-
-        }
+            if (!string.IsNullOrEmpty(input) && input == "sql")
+            {
+                var sqlConnection = new SqlConnection(input);
+                var dbCommand = new DbCommand(sqlConnection, sqlConnection.ConnectionString);
+                dbCommand.Execute();
+            }
+            else if (!string.IsNullOrEmpty(input) && input == "oracle")
+            {
+                var oracleConnection = new OracleConnection(input);
+                var dbCommand = new DbCommand(oracleConnection, oracleConnection.ConnectionString);
+                dbCommand.Execute();
+            }
+            else
+            {
+                throw new ArgumentException("Not a valid entry.");
+            }
     }
 }
