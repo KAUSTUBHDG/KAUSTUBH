954fd9c6c7fb7a44add9f32b412aca40c5111434
KAUSTUBHDG <63150992+KAUSTUBHDG@users.noreply.github.com>
2020-05-25 14:12:04 +0530

    Add files via upload

---
+++ date.sql
@@ -0,0 +1,14 @@
+select DATEPART(MONTH,'june 12 2011')
+
+select DATEPART(YEAR,'november 20 1992')
+
+select DATEADD(DAY,-74,GETDATE())
+
+select GETDATE()
+SELECT GETUTCDATE()
+select SYSDATETIME()
+
+select DATEDIFF(Day,'April 17, 1996','September 4, 2001')
+
+select DATEDIFF(MONTH,'December 25 1993',DATEADD(DAY,-2719,GETDATE()))
+
